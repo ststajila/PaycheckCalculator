b02cecf6b8fb8523299833600b84921ad1cd80a7
Stanislav <stazilastas@gmail.com>
2023-09-01 09:17:12 -0500

    Initial Commit

---
+++ PaycheckStatement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaycheckStatement
+//
+//  Created by STANISLAV STAJILA on 9/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PaycheckStatementUITests/PaycheckStatementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PaycheckStatementUITestsLaunchTests.swift
+//  PaycheckStatementUITests
+//
+//  Created by STANISLAV STAJILA on 9/1/23.
+//
+
+import XCTest
+
+final class PaycheckStatementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
