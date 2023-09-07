dd2d97d4dd70c98cb6dff76b8cf75c17f829cb38
Stanislav <stazilastas@gmail.com>
2023-09-07 09:50:47 -0500

    PaycheckCalculator 3.4

--- PaycheckStatement/ViewController.swift
+++ PaycheckStatement/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameInputOutlet: UITextField!
+    @IBOutlet weak var oftenOutlet: UIButton!
+    @IBOutlet weak var extraHoursWageOutlet: UITextField!
+    @IBOutlet weak var dateInputOutlet: UIDatePicker!
+    @IBOutlet weak var workDayHoursOutlet: UITextField!
+    @IBOutlet weak var hourlyWageOutlet: UITextField!
+//    var name : String?
+    let dateFormatter = DateFormatter()
+//    var date : String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func calculateAction(_ sender: Any) {
+//        name = nameInputOutlet.text
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        var date = dateFormatter.string(from: dateInputOutlet.date)
+        print(date)
+    }
+    
 
 }
 
