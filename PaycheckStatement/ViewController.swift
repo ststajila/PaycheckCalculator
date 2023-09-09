1a5646c9c596ed092907daefc80dc2f47ea505f8
stasik <stazilastas@gmail.com>
2023-09-08 22:15:11 -0500

    Paycheck Calculator 1.6

--- PaycheckStatement/ViewController.swift
+++ PaycheckStatement/ViewController.swift
@@ -13,12 +13,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var extraHoursWageOutlet: UITextField!
     @IBOutlet weak var dateInputOutlet: UIDatePicker!
     @IBOutlet weak var workDayHoursOutlet: UITextField!
+    @IBOutlet weak var hoursWorkedOutlet: UITextField!
     @IBOutlet weak var hourlyWageOutlet: UITextField!
     @IBOutlet weak var picOutlet: UISegmentedControl!
     var name : String?
     var timePayment: Int?
     let dateFormatter = DateFormatter()
-//    var date : String?
+    @IBOutlet weak var totalOutlet: UILabel!
+    @IBOutlet weak var spellAmountOutlet: UITextField!
     
     
     override func viewDidLoad() {
@@ -32,7 +34,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func calculateAction(_ sender: Any) {
         nameInputOutlet.resignFirstResponder()
         name = nameInputOutlet.text
-        print(name)
         
         dateFormatter.dateStyle = DateFormatter.Style.short
         var date = dateFormatter.string(from: dateInputOutlet.date)
@@ -43,12 +44,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
         } else {
             timePayment = 20
         }
+                
+
+    var wage = Double(hourlyWageOutlet.text!)
+        if let w = wage{
+            var extraHours = Double(extraHoursWageOutlet.text!)
+                if let ew = extraHours{
+                    var workDay = Double(workDayHoursOutlet.text!)
+                        if let wd = workDay{
+                            var hoursWorked = Double(hoursWorkedOutlet.text!)
+                                if let hw = hoursWorked{
+                                    if hw <= wd {
+                                       var total = Double(timePayment!) * hw * w
+                                        totalOutlet.text = "Your paycheck should be: \(total)"
+                                        
+                                    } else{
+                                       var total = Double(timePayment!) * ((wd * w) + ((hw - wd) * ew))
+                                        totalOutlet.text = "Your paycheck should be: \(total)"
+                                    }
+                                    
+                                } else {
+                                    hoursWorked = 0.0
+                                    print("Error: Use numbers!")
+                                }
+                        } else{
+                            workDay = 0.0
+                            print("Error: Use numbers!")
+                        }
+                } else{
+                    extraHours = 0.0
+                    print("Error: Use numbers!")
+                }
+        }else{
+            wage = 0.0
+            print("Error: Use numbers!")
+        }
         
-        print(timePayment)
-        
-        
-        
-        
+        print(spellAmountOutlet.text!)
+    
         
     }
 
